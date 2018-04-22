a3403f396d9fffb0e0a3559c0ada3779b899ce99
givip <pataridzegivi@gmail.com>
2018-04-22 22:01:18 +0300

    Added Regexp filter

--- Sources/Telegrammer/Filters/RegexpFilter.swift
+++ Sources/Telegrammer/Filters/RegexpFilter.swift
@@ -9,14 +9,26 @@ import Foundation
 
 public struct RegexpFilter: Filter {
     
+    let pattern: String
+    let options: NSRegularExpression.Options
+    
+    public init(pattern: String, options: NSRegularExpression.Options = []) {
+        self.pattern = pattern
+        self.options = options
+    }
+    
     public var name: String = "regexp"
     
     public func filter(message: Message) -> Bool {
-        //TODO: Implement
-        return false
+        guard let text = message.text else { return false }
+        guard let regexp = try? NSRegularExpression(pattern: pattern, options: options) else { return false }
+        let range = NSRange(location: 0, length: text.count)
+        return regexp.numberOfMatches(in: text, options: [], range: range) != 0
     }
 }
 
 public extension Filters {
-    static var regexp = Filters(filter: RegexpFilter())
+    static func regexp(pattern: String, options: NSRegularExpression.Options = []) -> Filters {
+        return Filters(filter: RegexpFilter(pattern: pattern, options: options))
+    }
 }
