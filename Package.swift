48889903ad7f30ea2fe3723202f32b45ddf4b29f
givip <pataridzegivi@gmail.com>
2018-04-22 09:27:15 +0300

    Clean-up dependencies

--- Package.swift
+++ Package.swift
@@ -14,9 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-//        .package(url: "https://github.com/apple/swift-nio.git", from: "1.2.0"),
-//        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "1.0.0"),
-//
+        
         .package(url: "https://github.com/vapor/core.git", from: "3.1.0"),
         .package(url: "https://github.com/vapor/http.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/crypto.git", from: "3.1.0"),
