9610cc66949dd925282f36488c3ea7ae889b8450
givip <pataridzegivi@gmail.com>
2018-05-18 23:12:36 +0300

    Improved longpolling mechanism, now specify qty of retries when request fails and is first getUpdates must be ignored (cleanStart)?

--- Sources/Telegrammer/Network/Longpolling.swift
+++ Sources/Telegrammer/Network/Longpolling.swift
@@ -6,27 +6,30 @@
 //
 
 import Foundation
+import LoggerAPI
+import HeliumLogger
 import HTTP
 
 public class Longpolling: Connection {
-	
-	public static let PollInterval              = TimeAmount.seconds(0)
-	public static let Timeout                   = 20
-	public static let Clean                     = false
-	public static let BootstrapRetries: Int?    = nil
-	public static let ReadLatency               = TimeAmount.seconds(2)
-	public static let AllowedUpdates: [String]? = nil
-	public static let Limit: Int?               = nil
 	
 	public var bot: Bot
 	public var dispatcher: Dispatcher
 	public var worker: Worker
 	public var running: Bool
 	
-	private var lastUpdate: Update?
-	private var pollingInterval = PollInterval
-	private var pollingPromise: Promise<Void>?
-	
+    public var allowedUpdates: [String]? = nil
+    public var limit: Int? = nil
+    public var bootstrapRetries: Int? = nil
+    public var cleanStart: Bool = false
+    public var pollingTimeout: Int = 20
+    public var pollingInterval: TimeAmount = TimeAmount.seconds(2)
+    
+    private var lastUpdate: Update?
+    private var connectionRetries: Int = 0
+    private var isFirstRequest: Bool = true
+    
+    private var pollingPromise: Promise<Void>?
+    
 	public init(bot: Bot, dispatcher: Dispatcher, worker: Worker = MultiThreadedEventLoopGroup(numThreads: 1)) {
 		self.bot = bot
 		self.dispatcher = dispatcher
@@ -34,18 +37,11 @@ public class Longpolling: Connection {
 		self.running = false
 	}
 	
-	public func start(timeout: Int = Timeout,
-					  pollInterval: TimeAmount = PollInterval,
-					  clean: Bool = Clean,
-					  bootstrapRetries: Int? = BootstrapRetries,
-					  readLatency: TimeAmount = ReadLatency,
-					  allowedUpdates: [String]? = AllowedUpdates,
-					  limit: Int? = Limit) throws -> Future<Void> {
+	public func start() throws -> Future<Void> {
 		self.running = true
-		self.pollingInterval = pollInterval
 		
 		let promise = worker.eventLoop.newPromise(Void.self)
-		let params = Bot.GetUpdatesParams(offset: nil, limit: limit, timeout: timeout, allowedUpdates: allowedUpdates)
+		let params = Bot.GetUpdatesParams(offset: nil, limit: limit, timeout: pollingTimeout, allowedUpdates: allowedUpdates)
 		
 		worker.eventLoop.execute {
 			self.longpolling(with: params)
@@ -67,15 +63,18 @@ public class Longpolling: Connection {
 		do {
 			let updates = try self.bot.getUpdates(params: requestBody).wait()
 			if !updates.isEmpty {
-				dispatcher.enqueue(updates: updates)
+                if !cleanStart || !(cleanStart && isFirstRequest) {
+                    dispatcher.enqueue(updates: updates)
+                }
 				if let last = updates.last {
 					requestBody.offset = last.updateId + 1
 				}
 			}
+            isFirstRequest = false
 			self.scheduleLongpolling(with: requestBody)
 		} catch {
-			running = false
-			self.pollingPromise?.fail(error: error)
+            Log.error(error.localizedDescription)
+            retryRequest(with: params, after: error)
 		}
 	}
 	
@@ -84,4 +83,20 @@ public class Longpolling: Connection {
 			self.longpolling(with: params)
 		}
 	}
+    
+    private func retryRequest(with params: Bot.GetUpdatesParams, after error: Error) {
+        guard let maxRetries = bootstrapRetries, connectionRetries < maxRetries else {
+            running = false
+            Log.debug("Failed connection after \(connectionRetries) retries")
+            pollingPromise?.fail(error: error)
+            return
+        }
+        
+        connectionRetries += 1
+        Log.debug("Retry \(connectionRetries) after failed request")
+        
+        _ = worker.eventLoop.scheduleTask(in: pollingInterval, { () -> Void in
+            self.longpolling(with: params)
+        })
+    }
 }
