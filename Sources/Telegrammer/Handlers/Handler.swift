16635c81b7e460953c969ffa10c234db1b9594b9
givip <pataridzegivi@gmail.com>
2018-05-30 23:57:06 +0300

    Fixed compile error

--- Sources/Telegrammer/Handlers/Handler.swift
+++ Sources/Telegrammer/Handlers/Handler.swift
@@ -9,7 +9,7 @@ import HTTP
 
 public typealias HandlerCallback = (_ update: Update, _ updateQueue: Worker?, _ jobQueue: Worker?) throws -> Void
 
-public typealias ErrorHandlerCallback = (_ bot: Bot, _ update: Update, _ error: TelegramError?) -> Void
+public typealias ErrorHandlerCallback = (_ bot: Bot, _ update: Update, _ error: Error) -> Void
 
 public protocol Handler {
     func check(update: Update) -> Bool
