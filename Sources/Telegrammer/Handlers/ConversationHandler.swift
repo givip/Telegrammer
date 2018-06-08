547c99f5d57ad73783899c928543eb75ffe2fb80
givip <pataridzegivi@gmail.com>
2018-06-08 18:43:17 +0300

    Adding conversation Handler

---
+++ Sources/Telegrammer/Handlers/ConversationHandler.swift
@@ -0,0 +1,27 @@
+//
+//  ConversationHandler.swift
+//  Telegrammer
+//
+//  Created by Givi Pataridze on 08.06.2018.
+//
+
+import Foundation
+
+//public class ConversationHandler: Handler {
+//	
+//	public var name: String
+//	
+//	let entryPoints: [Handler]
+//	let states: [String: [Handler]]
+//	let fallbacks: [Handler]
+//	let timeoutBehavior: [Handler]
+//	
+//	public func check(update: Update) -> Bool {
+//		
+//	}
+//	
+//	public func handle(update: Update, dispatcher: Dispatcher) throws {
+//		
+//	}
+//	
+//}
