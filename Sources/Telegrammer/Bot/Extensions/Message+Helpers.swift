fe947812ba4393a81bad820f55a0265c621fb641
givip <pataridzegivi@gmail.com>
2018-06-17 09:49:41 +0300

    Added some message helpers

--- Sources/Telegrammer/Bot/Extensions/Message+Helpers.swift
+++ Sources/Telegrammer/Bot/Extensions/Message+Helpers.swift
@@ -6,8 +6,24 @@
 //
 
 public extension Message {
-    func reply(_ text: String, from bot: Bot) throws {
-        let params = Bot.SendMessageParams(chatId: .chat(chat.id), text: text)
+    func reply(text: String, from bot: Bot, parseMode: ParseMode? = nil, replyMarkup: ReplyMarkup? = nil) throws {
+        let params = Bot.SendMessageParams(chatId: .chat(chat.id),
+                                           text: text,
+                                           parseMode: parseMode,
+                                           replyMarkup: replyMarkup)
         try bot.sendMessage(params: params)
+    }
+    
+    func edit(text: String, from bot: Bot, parseMode: ParseMode? = nil, replyMarkup: InlineKeyboardMarkup? = nil) throws {
+        let params = Bot.EditMessageTextParams(chatId: .chat(chat.id),
+                                               text: text,
+                                               parseMode: parseMode,
+                                               replyMarkup: replyMarkup)
+        try bot.editMessageText(params: params)
+    }
+    
+    func delete(from bot: Bot) throws {
+        let params = Bot.DeleteMessageParams(chatId: .chat(chat.id), messageId: messageId)
+        try bot.deleteMessage(params: params)
     }
 }
