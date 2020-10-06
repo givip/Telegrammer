cc49bfecc2d5cc07f4162dddd1982813c4859fee
givip <pataridzegivi@gmail.com>
2020-10-06 15:05:31 +0400

    Minor Fix

--- Sources/Telegrammer/Types/ParseMode.swift
+++ Sources/Telegrammer/Types/ParseMode.swift
@@ -13,6 +13,8 @@ import Foundation
  [Formatting Options](https://core.telegram.org/bots/api#formatting-options)
  */
 public enum ParseMode: String, Codable {
+    case markdownV2 = "MarkdownV2"
+    @available(*, deprecated, message: "This is a legacy mode, retained for backward compatibility.", renamed: "markdownV2")
     case markdown  = "Markdown"
     case html      = "HTML"
     case undefined
