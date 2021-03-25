fc02a8a43e41b0ed717d07ed111986739a0286bd
gh.guo <747237971@qq.com>
2021-03-25 15:39:16 +0800

    Update Package.swift
    10_15

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Telegrammer",
     platforms: [
-        .macOS(.v10_14)
+        .macOS(.v10_15)
     ],
     products: [
         .library(name: "Telegrammer", targets: ["Telegrammer"]),
